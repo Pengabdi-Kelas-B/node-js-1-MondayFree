3ac3a35b3bcf27a2785f3c54c5fb861eba7d9f97
Luji Ramdhan <lrdn03@gmail.com>
2024-10-26 18:07:24 +0700

    feat: Finishing ext-sorter

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const fs = require("fs/promises")
 const readline = require('node:readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
-// readline helper
-const getInput = title => {
-  return new Promise((resolve, reject) => {
-    rl.question(title, input => resolve(input));
+// create readline instance
+const createReadLine = () => {
+  return readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
 }
 
+
+// readline helper
+const generateGetInput = rl => {
+  return title => {
+    return new Promise((resolve, reject) => {
+      rl.question(title, input => resolve(input));
+    });
+  }
+}
+
+
+// create app
 const app = {}
+
 
 // make folder
 app.makeFolder = () => {
+  const rl = createReadLine();
   rl.question("Masukan Nama Folder : ", async (folderName) => {
     try {
       await fs.mkdir(__dirname + `/${folderName}`);
@@ -28,8 +39,11 @@ app.makeFolder = () => {
   })
 }
 
+
 // make file
 app.makeFile = async () => {
+  const rl = createReadLine();
+  const getInput = generateGetInput(rl);
   try {
     let extensi = await getInput('Masukan jenis file : ');
     extensi = extensi.trim();  
@@ -51,8 +65,28 @@ app.makeFile = async () => {
   } catch(err) {
     console.log(err.message);
   }
-
   rl.close();
+};
+
+
+// ext sorter
+app.extSorter = async () => {
+  try {
+    const unOrganizeFolder = await fs.readdir('unorganize_folder');
+    if(unOrganizeFolder.length === 0) throw new Error("Tidak ada file di folder unorganize_folder");
+    
+    for(const file of unOrganizeFolder) {
+      const root = await fs.readdir('.');
+      let ext = file.split(".");
+      ext = ext[ext.length - 1];
+      if(!root.includes(ext)) await fs.mkdir(__dirname + `/${ext}`);
+      await fs.rename(__dirname + `/unorganize_folder/${file}`, __dirname + `/${ext}/${file}`);
+    }
+
+    console.log("Semua file berhasil dirapihkan");
+  } catch(err) {
+    console.log(err.message);
+  }
 };
 
 
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const app = require("./app.js");
 
 const args = process.argv.slice(2);
-
 const command = args[0];
 
 switch (command) {
   case "make-folder":
     app.makeFolder()
     break;
-
   case 'make-file' :
     app.makeFile();
     break;
-
+  case 'ext-sorter' :
+    app.extSorter();
+    break;
   default:
     throw Error("Invalid command")
-    break;
 }
